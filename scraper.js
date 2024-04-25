84ccb8a7711005ccc9f097d4ff1e55f47c3e5ef0
Masbull <93698790+MNanangUtomo12@users.noreply.github.com>
2024-04-25 22:58:55 +0700

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1031,7 +1031,7 @@ async function checkProxy(url, userAgent) {
             },
             timeout: 5000 // Timeout request dalam milidetik
         });
-        if (response.status === 30000) {
+        if (response.status === 1000) {
             console.log(`Proxy aktif ditemukan: ${url}`);
             activeProxies.push(url);
             if (activeProxies.length === totalProxiesToSave) {
